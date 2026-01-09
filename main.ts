6b0cd602526ae8bd9b371b04aa56dc659d714528
Brendan O'Rourke <brendan.orourke@stu.ocsb.ca>
2026-01-09 13:05:14 -0500

    Deleted spaces

--- main.ts
+++ main.ts
@@ -10,22 +10,22 @@ let scoreP2 = 9999
 let startTime = 0
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 
-// Setup
+ // Setup
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.showString("Player 1") // Start as Player 1
 
-// Switch Player by Shaking Microbit
+ // Switch Player by Shaking Microbit
 input.onGesture(Gesture.Shake, function () {
-if (playerMode == 1) {
+    if (playerMode == 1) {
 playerMode = 2
 basic.showString("Player 2")
-} else {
+    } else {
 playerMode = 1
 basic.showString("Player 1")
 }
 })
 
-// Start game press Button B 
+ // Start game press Button B 
 input.onButtonPressed(Button.B, function () {
 basic.clearScreen()
 strip.showColor(neopixel.colors(NeoPixelColors.Red))
@@ -34,24 +34,24 @@ strip.showColor(neopixel.colors(NeoPixelColors.Green))
 startTime = control.millis()
 })
 
-// Results Button A 
+ // Results Button A 
 input.onButtonPressed(Button.A, function () {
-if (startTime > 0) {
+    if (startTime > 0) {
 let result = control.millis() - startTime
 startTime = 0
 basic.showNumber(result)
 
-// Check High Score for the ACTIVE player
-if (playerMode == 1) {
-if (result < scoreP1) {
+ // Check High Score for the ACTIVE player
+    if (playerMode == 1) {
+    if (result < scoreP1) {
 scoreP1 = result
 strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
 basic.showString("Player 1 BEST!")
 }
 } else {
- if (result < scoreP2) {
- scoreP2 = result
- strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    if (result < scoreP2) {
+scoreP2 = result
+strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
 basic.showString("Player 2 BEST!")
 }
 }
