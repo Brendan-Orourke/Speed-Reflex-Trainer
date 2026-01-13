175b08477160e5ae756e518901f3b4dae93788d9
Brendan O'Rourke <brendan.orourke@stu.ocsb.ca>
2026-01-13 12:44:25 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,44 +17,50 @@ strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.showString("Player 1")
 
 // Switch Player by Shaking Microbit
-    input.onGesture(Gesture.Shake, function () {
-// Swaps between 1 and 2 automatically
-    if (currentPlayer == 1) {   
-        currentPlayer = 2   
-    } else {    
+input.onGesture(Gesture.Shake, function () {
+    // Swaps between 1 and 2 automatically
+    if (currentPlayer == 1) {
+        currentPlayer = 2
+    } else {
         currentPlayer = 1
-    }   
-    
-// Start game press Button B 
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(randint(1000, 3000))
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    startTime = control.millis()
+    }
+
+    // Start game press Button B
+    input.onButtonPressed(Button.B, function () {
+        basic.clearScreen()
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(randint(1000, 3000))
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        startTime = control.millis()
+    })
+
+
+    // Results Button A
+    input.onButtonPressed(Button.A, function () {
+        if (startTime > 0) {
+            let result = control.millis() - startTime
+            startTime = 0
+            basic.showNumber(result)
+
+
+            // Check High Score for Player 1
+            if (playerMode == 1 && result < scoreP1) {
+                scoreP1 = result
+                strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+                basic.showString("Player 1 BEST!")
+            }
+
+
+            // Check High Score for Player 2
+            if (playerMode == 2 && result < scoreP2) {
+                scoreP2 = result
+                strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+                basic.showString("Player 2 BEST!")
+            }
+
+
+            basic.pause(1000)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        }
+    })
 })
-
-// Results Button A 
-input.onButtonPressed(Button.A, function () {
-    if (startTime > 0) {
-        let result = control.millis() - startTime
-        startTime = 0
-        basic.showNumber(result)
-
-        // Check High Score for Player 1
-        if (playerMode == 1 && result < scoreP1) {
-            scoreP1 = result
-            strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-            basic.showString("Player 1 BEST!")
-        }
-
-        // Check High Score for Player 2
-        if (playerMode == 2 && result < scoreP2) {
-            scoreP2 = result
-            strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-            basic.showString("Player 2 BEST!")
-        }
-
-        basic.pause(1000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    }})})
